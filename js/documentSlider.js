3b7fb9036f5f6b3b7b359b119715319abef1b2d4
Oleh Fedorov <akademuk24@gmail.com>
2020-07-06 12:28:20 +0300

    a

--- js/documentSlider.js
+++ js/documentSlider.js
@@ -3,4 +3,21 @@ $(".documentSlider").slick({
   dots: true,
   slidesToShow: 3,
   slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+      },
+    },
+  ],
 });
